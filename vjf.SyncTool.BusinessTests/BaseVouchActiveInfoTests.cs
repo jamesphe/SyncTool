827b476111570aa906d21b0ca1a1b59233874511
jamesphe <james_phe@126.com>
2015-04-24 16:51:34 +0800

    syncTool init

---
+++ vjf.SyncTool.BusinessTests/BaseVouchActiveInfoTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vjf.SyncTool.Business;
+using NUnit.Framework;
+namespace vjf.SyncTool.Business.Tests
+{
+    [TestFixture()]
+    public class BaseVouchActiveInfoTests
+    {
+        [Test()]
+        public void GetByIdentificationTest()
+        {
+            var curObj = BaseVouchActiveInfo.GetByIdentification("VA142289060146146798");
+
+        }
+    }
+}
---
+++ vjf.SyncTool.UI/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿#region using declarations
+
+using System;
+
+using Csla;
+
+using vjf.SyncTool.Business;
+
+#endregion
+namespace vjf.SyncTool.UI
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
